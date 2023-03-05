90f02b560737f4fc8d1a175389e4839293021357
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-03-05 18:26:54 +0300

    Стек - сделано

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,64 +1,150 @@
-import React, { FormEvent, ChangeEvent, useEffect, useState } from "react";
+import React, { FormEvent, MouseEvent, ChangeEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { Stack } from "./stack";
+import { TElement } from "./types";
+import { ElementStates } from "../../types/element-states";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 import styles from "./stack-page.module.css";
 
+const stack = new Stack<TElement<string>>();
+
 export const StackPage: React.FC = () => {
+  const [inputString, setInputString] = useState<string>("");
+  const [isWorking, setIsWorking] = useState({
+    addBtn: false,
+    removeBtn: false,
+    resetBtn: false,
+  });
+
+  const [elements, setElements] = useState<TElement<string>[]>([]);
+
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setInputString(event.target.value);
+  };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  }
+  };
+
+  const handleAddElement = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsWorking({
+      addBtn: true,
+      removeBtn: false,
+      resetBtn: false,
+    });
+    stack.push({
+      value: inputString,
+      state: ElementStates.Changing,
+    });
+    setInputString("");
+    setElements([...stack.elements]);
+
+    setTimeout(() => {
+      stack.setElementByIndex(stack.size() - 1, {
+        value: inputString,
+        state: ElementStates.Default,
+      });
+      setElements([...stack.elements]);
+      setIsWorking({
+        addBtn: false,
+        removeBtn: false,
+        resetBtn: false,
+      });
+    }, SHORT_DELAY_IN_MS);
+  };
+
+  const handleRemoveElement = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsWorking({
+      addBtn: false,
+      removeBtn: true,
+      resetBtn: false,
+    });
+
+    stack.setElementByIndex(stack.size() - 1, {
+      value: stack.elements[stack.size() - 1].value,
+      state: ElementStates.Changing,
+    });
+
+    setElements([...stack.elements]);
+
+    setTimeout(() => {
+      stack.pop();
+      setElements([...stack.elements]);
+      setIsWorking({
+        addBtn: false,
+        removeBtn: false,
+        resetBtn: false,
+      });
+    }, SHORT_DELAY_IN_MS);
+  };
 
   const handleReset = (event: FormEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    setIsWorking(false);
-    setInitArr(randomArr());
-    setCurrentStep(null);
+    stack.clear();
+    setElements(stack.elements);
+    setIsWorking({
+      addBtn: false,
+      removeBtn: false,
+      resetBtn: false,
+    });
   };
 
   return (
     <SolutionLayout title="Стек">
       <div className={styles.container}>
         <form className={styles.form} onSubmit={handleSubmit}>
-        <Input
-            type={"number"}
-            name={"fiboInput"}
+          <Input
+            placeholder="Введите значение"
             extraClass={styles.input}
             isLimitText={true}
-            max={19}
-            onChange={(event) =>
-              setInputString((event.target as HTMLButtonElement).value)
-            }
+            maxLength={4}
+            onChange={handleInput}
+            value={inputString}
           />
-        <Button
+          <Button
             text="Добавить"
-            extraClass={`${styles.button} ${styles.button_asc}`}
-            type="submit"
-            name="buttonAsc"
-            isLoader={isWorking && sortDirection === Direction.Ascending}
-            sorting={Direction.Ascending}
-            disabled={isWorking}
+            extraClass={`${styles.button} ${styles.button_add}`}
+            onClick={handleAddElement}
+            isLoader={isWorking.addBtn}
+            disabled={isWorking.removeBtn || !inputString}
           />
           <Button
             text="Удалить"
-            extraClass={`${styles.button} ${styles.button_desc}`}
-            type="submit"
-            name="buttonDesc"
-            isLoader={isWorking && sortDirection === Direction.Descending}
-            sorting={Direction.Descending}
-            disabled={isWorking}
+            extraClass={`${styles.button} ${styles.button_remove}`}
+            onClick={handleRemoveElement}
+            isLoader={isWorking.removeBtn}
+            disabled={isWorking.addBtn || elements.length === 0}
           />
           <Button
             text="Очистить"
             extraClass={`${styles.button} ${styles.button_reset}`}
             type="reset"
             onClick={(e) => handleReset(e)}
-            disabled={isWorking}
+            isLoader={isWorking.resetBtn}
+            disabled={isWorking.addBtn || isWorking.removeBtn || elements.length === 0}
           />
         </form>
+
+        <div className={styles.circles}>
+          {elements.length > 0 &&
+            elements.map((element, index) => {
+              return (
+                <Circle
+                  key={index}
+                  letter={element.value}
+                  state={element.state}
+                  head={index === elements.length - 1 ? "top" : ""}
+                />
+              );
+            })}
+        </div>
       </div>
     </SolutionLayout>
   );
--- src/components/stack-page/stack.ts
+++ src/components/stack-page/stack.ts
@@ -4,19 +4,46 @@ export interface IStack<T> {
   push: (item: T) => void;
   pop: () => void;
   peak: () => T | null;
+  size: () => number;
+  clear: () => void;
+  setElementByIndex: (index: number, element: T) => void;
 }
 
-export class Stack implements IStack<T> {
 
-  push() {
 
+export class Stack<T> implements IStack<T> {
+
+  private container: T[] = [];
+
+  get elements() {
+    return [...this.container];
+  }
+
+  push = (item: T): void => {
+    this.container.push(item);
   };
 
-  pop() {
-
+  pop = (): void => {
+    if (this.container.length > 0) {
+      this.container.pop();
+    }
   };
 
-  peak() {
-    
+  peak = (): T | null => {
+    if (this.container.length > 0) {
+      const index = this.size() - 1;
+      return this.container[index];
+    }
+    return null;
+  };
+
+  size = () => this.container.length;
+
+  clear = () => {
+    this.container = [];
+  }
+
+  setElementByIndex = (index: number, element: T) => {
+    this.container[index] = element;
   }
 }
