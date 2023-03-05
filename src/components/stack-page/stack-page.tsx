901d7598e17fec79a677a4d1930534439c9d746b
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-03-05 09:14:58 +0300

    first commit

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { FormEvent, ChangeEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import styles from "./stack-page.module.css";
+
 export const StackPage: React.FC = () => {
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
   return (
     <SolutionLayout title="Стек">
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
 
+        </form>
+      </div>
     </SolutionLayout>
   );
 };
