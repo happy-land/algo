9a57654527b7a969c7f382b7531c88f8ff055ceb
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-03-05 18:50:52 +0300

    first commit

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { FormEvent, MouseEvent, ChangeEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+import styles from "./queue-page.module.css";
+
 export const QueuePage: React.FC = () => {
+  const [inputString, setInputString] = useState<string>("");
+  const [isWorking, setIsWorking] = useState({
+    addBtn: false,
+    removeBtn: false,
+    resetBtn: false,
+  });
+
   return (
     <SolutionLayout title="Очередь">
 
