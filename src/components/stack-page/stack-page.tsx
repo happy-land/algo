e358498349d1807bc432ec3e58179717699563cb
Ruslan <35293403+happy-land@users.noreply.github.com>
2023-03-05 10:09:36 +0300

    stack draft

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,5 +1,8 @@
 import React, { FormEvent, ChangeEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 import styles from "./stack-page.module.css";
 
@@ -9,11 +12,52 @@ export const StackPage: React.FC = () => {
     event.preventDefault();
   }
 
+  const handleReset = (event: FormEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsWorking(false);
+    setInitArr(randomArr());
+    setCurrentStep(null);
+  };
+
   return (
     <SolutionLayout title="Стек">
       <div className={styles.container}>
         <form className={styles.form} onSubmit={handleSubmit}>
-
+        <Input
+            type={"number"}
+            name={"fiboInput"}
+            extraClass={styles.input}
+            isLimitText={true}
+            max={19}
+            onChange={(event) =>
+              setInputString((event.target as HTMLButtonElement).value)
+            }
+          />
+        <Button
+            text="Добавить"
+            extraClass={`${styles.button} ${styles.button_asc}`}
+            type="submit"
+            name="buttonAsc"
+            isLoader={isWorking && sortDirection === Direction.Ascending}
+            sorting={Direction.Ascending}
+            disabled={isWorking}
+          />
+          <Button
+            text="Удалить"
+            extraClass={`${styles.button} ${styles.button_desc}`}
+            type="submit"
+            name="buttonDesc"
+            isLoader={isWorking && sortDirection === Direction.Descending}
+            sorting={Direction.Descending}
+            disabled={isWorking}
+          />
+          <Button
+            text="Очистить"
+            extraClass={`${styles.button} ${styles.button_reset}`}
+            type="reset"
+            onClick={(e) => handleReset(e)}
+            disabled={isWorking}
+          />
         </form>
       </div>
     </SolutionLayout>
---
+++ src/components/stack-page/stack.ts
@@ -0,0 +1,22 @@
+
+
+export interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+}
+
+export class Stack implements IStack<T> {
+
+  push() {
+
+  };
+
+  pop() {
+
+  };
+
+  peak() {
+    
+  }
+}
